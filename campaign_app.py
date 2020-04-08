00a0e5d7acb1d3dd059ff406d0749130c8def5e9
Ivy Wang <28444726+Ivyw1219@users.noreply.github.com>
2020-04-08 18:27:07 +0200

    Update campaign_app.py

--- campaign_app.py
+++ campaign_app.py
@@ -30,12 +30,10 @@ def main():
     df = load_data()
 
     st.title("Marketing Campaign Report")
-    #text ="""\
-    #![]('https://github.com/Ivyw1219/Marketing_Campaign_APP/blob/master/header.png')
-    #"""
-   # st.markdown(text)
-    img= Image.open("https://github.com/Ivyw1219/Marketing_Campaign_APP/blob/master/header.png")
-    st.image(img,width =800)
+    text ="""\
+    ![]('https://github.com/Ivyw1219/Marketing_Campaign_APP/blob/master/header.png')
+    """
+    st.markdown(text)
 
     # add sth into sidebar
     text = """
@@ -115,12 +113,13 @@ def main():
         st.markdown(text)
 
     st.sidebar.subheader("About Author")
-    #text = """\
-    # Data Scientist  [**Ivy Wang**](https://www.linkedin.com/in/ivy-w-81871716b/)
-    # """
-    #st.sidebar.markdown(text)
-    img= Image.open("https://github.com/Ivyw1219/Marketing_Campaign_APP/blob/master/profile.png")
-    st.image(img,width =300)
+    text = """\
+    Data Scientist  [**Ivy Wang**](https://www.linkedin.com/in/ivy-w-81871716b/)\
+    ![]("https://github.com/Ivyw1219/Marketing_Campaign_APP/blob/master/profile.png")
+     """
+    st.sidebar.markdown(text)
+
+   
 
 
 
